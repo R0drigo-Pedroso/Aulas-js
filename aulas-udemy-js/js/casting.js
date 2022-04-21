8b9f6c1ff029810dc3e1d2de39a6580880bf9e45
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-04-21 19:02:48 -0300

    Aulas Udemy
    aprendendo e praticando conceitos de js

---
+++ aulas-udemy-js/js/casting.js
@@ -0,0 +1,34 @@
+
+/* tranformando string em soma numerica */
+/*
+var variavel1 = prompt("Digite um número");
+var variavel2 = prompt("Digite outro número");
+
+// variavel1 = parseInt(variavel1); //converte para inteiro
+// variavel2 = parseInt(variavel2); //converte para inteiro
+
+// variavel1 = parseInt(variavel1); //converte para inteiro
+// variavel2 = parseFloat(variavel2);  //converte para fração
+
+// document.write(variavel1 + variavel2);
+
+console.log (variavel1);
+console.log (variavel2); 
+
+/* ---------------------------------------------- */
+
+/* tranformando numero em string - concatenação */
+
+var variavel1 = 10
+var variavel2 = 20
+
+// variavel1 = parseInt(variavel1); //converte para inteiro
+// variavel2 = parseInt(variavel2); //converte para inteiro
+
+// variavel1 = parseInt(variavel1); //converte para inteiro
+// variavel2 = parseFloat(variavel2);  //converte para fração
+
+document.write(variavel1.toString() + variavel2.toString());
+
+console.log (variavel1);
+console.log (variavel2);
---
+++ aulas-udemy-js/js/script.js
@@ -0,0 +1,11 @@
+
+var nota = prompt ("Digite a nota do aluno: ")
+var media = 7
+
+if (nota >= media)  {
+    document.write("Aprovado")
+} else {
+    document.write("Reprovado")
+}
+
+//o operado === (identico) leva em consideração o tipo de operação que esta sendo feita
