167a8b42214c43242db7b02d475f335af374812a
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-04-26 10:14:36 -0300

    Atualizando DOM
    Adicionando nova tag.
    agregando css com js

--- 06-Dom/manipulandodom.js
+++ 06-Dom/manipulandodom.js
@@ -16,13 +16,16 @@ textContent - altera o conteúdo de um elemento
 const titulo = document.getElementById('titulo-principal');
 console.log(titulo);
 
+
 // selecionado pelo TAG
 const pagina = document.querySelector('body');
 console.log(pagina);
 
+
 // selecionado todas as TAGS H2
 const subtitulo = document.querySelectorAll('h2');
 console.log(subtitulo);
+
 
 // Modificando elementos
 titulo.textContent = 'Olá Mundo!'; // modifica o texto textContent muda apenas o texto
@@ -35,8 +38,40 @@ const legenda = document.querySelector('figcaption'); // selecionando o elemento
 console.log(legenda); // mostra o elemento
 legenda.innerHTML = '<b>Ilustração do DOM</b>'; // innertHTML modifica o texto do elemento e adiciona o HTML dentro
 
-// CSS via JS
 
+// CSS via JS
 titulo.style.textAlign = 'center'; // modifica o textAlign
 titulo.style.color = 'white'; // modifica o color
 titulo.style.backgroundImage = 'linear-gradient(to right, #f00, #0f0)'; // modifica o backgroundImage
+
+
+
+// Configurando Editores
+const listaEditores = document.querySelector("#lista-editores"); // selecionando o elemento
+
+const primeiro = listaEditores.querySelector("li:first-child"); // selecionando o primeiro elemento
+const segundo = listaEditores.querySelector("li:nth-child(2)"); // selecionando o segundo elemento
+const ultimo = listaEditores.querySelector("li:last-child"); // selecionando o ultimo elemento
+
+/*
+primeiro.style.color = 'red';
+segundo.style.color = 'green';
+terceiro.style.color = 'yellow';
+ultimo.style.color = 'blue';
+*/
+
+ultimo.classList.add('destaque-item'); // adiciona a classe destaque-item
+
+
+//Configurando Referencias
+const links = document.querySelectorAll('ul li a'); // selecionando todos os elementos "ul li a"
+console.log(links);
+
+//loop de for - para abrir a pagina em outra tela.
+for(let i = 0; i < links.length; i++){ // links.length - conta quantos elementos tem na lista
+    links[i].setAttribute('target', '_blank'); // adiciona o atributo target
+    links[i].style.color = 'blue'; // adiciona o estilo
+} 
+// i = indice ou seja, e o i++ é o próximo elemento da lista (i = contador)
+
+// links[0].style.color = 'red'; // selecionando o primeiro elemento, trocando a cor
