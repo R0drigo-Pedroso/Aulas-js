f245f460e2420e52ddb3c652eafd4a9bc59c8040
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-04-28 22:29:03 -0300

    Atualização

--- 08-eventos.html/js/eventos.js
+++ 08-eventos.html/js/eventos.js
@@ -1,15 +1,12 @@
-for (let i = 0; i <100; i++) {
-    console.log(i);
-}
+// for (let i = 0; i <100; i++) {
+//     console.log(i);
+// }
 
-document.write(i);
+// document.write(i);
 
-
-
-/*
 let nome = document.getElementById("nome");
 let resultado = document.getElementById("resultado");
 
 function clique() {
     resultado.textContent = "Olá, " + nome.value;
-};*/
+};
